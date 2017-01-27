9383f6be1a8a723a2a1c57525fa8a4dabf44538c
dinar_dalvi <dinar@applatix.com>
2017-01-26 17:14:08 -0800

    added fixture

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+
 # Copy the jmx to master.
 cp /src/jmeter-test.jmx /jmeter/apache-jmeter-3.1/bin/
 
+echo "tomcat_ip= " $1
+echo "slave1_ip= " $2
+echo "slave2_ip= " $3
+echo "slave3_ip= " $4
 
 cd /jmeter/apache-jmeter-3.1/bin
 
-./jmeter -n -t jmeter-test.jmx -R172.17.0.3,172.17.0.4,172.17.0.5
+ls -al
+
+#./jmeter -n -t jmeter-test.jmx -R172.17.0.3,172.17.0.4,172.17.0.5
